a816618de5a1c263622902ff48466019e66bde7a
developer-suhag <developersuhag@gmail.com>
2023-01-10 20:19:23 +0600

    add job form design

--- src/pages/Dashboard/AddJob.js
+++ src/pages/Dashboard/AddJob.js
@@ -1,5 +1,275 @@
+import {
+  Box,
+  Button,
+  Container,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { useFieldArray, useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { TiDelete } from "react-icons/ti";
+import { AiOutlineSend } from "react-icons/ai";
+import { useSelector } from "react-redux";
+
 const AddJob = () => {
-  return <div>Add a Job</div>;
+  const {
+    user: { companyName },
+  } = useSelector((state) => state.auth);
+
+  const { handleSubmit, register, control } = useForm({
+    defaultValues: {
+      companyName,
+    },
+  });
+  const navigate = useNavigate();
+
+  const {
+    fields: skillFields,
+    append: skillAppend,
+    remove: skillRemove,
+  } = useFieldArray({ control, name: "skills" });
+
+  const {
+    fields: responsibilityFields,
+    append: responsibilityAppend,
+    remove: responsibilityRemove,
+  } = useFieldArray({ control, name: "responsibilities" });
+
+  const {
+    fields: requirementFields,
+    append: requirementAppend,
+    remove: requirementRemove,
+  } = useFieldArray({ control, name: "requirements" });
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+  return (
+    <>
+      {/* {isLoading && <LinearProgress />} */}
+      <Container>
+        <Box
+          sx={{
+            py: 3,
+            px: 4,
+            boxShadow: 1,
+            borderRadius: 2,
+          }}
+        >
+          <Box sx={{ mb: 6 }}>
+            <Typography
+              sx={{ fontSize: { xs: 24, md: 30 }, textAlign: "center", my: 2 }}
+              color="secondary"
+              variant="h3"
+            >
+              Add a new Position
+            </Typography>
+          </Box>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Stack sx={{ my: 2 }} direction="row" gap={4}>
+              <TextField
+                fullWidth
+                type="text"
+                variant="filled"
+                label="Position"
+                {...register("position")}
+              />
+              <TextField
+                fullWidth
+                type="text"
+                variant="filled"
+                label="Company Name"
+                {...register("companyName")}
+              />
+            </Stack>
+            <Stack sx={{ my: 2 }} direction="row" gap={4}>
+              <TextField
+                fullWidth
+                type="text"
+                variant="filled"
+                label="Experience"
+                {...register("experience")}
+              />
+              <TextField
+                fullWidth
+                type="text"
+                variant="filled"
+                label=" Work Level"
+                {...register("workLevel")}
+              />
+            </Stack>
+            <Stack sx={{ my: 2 }} direction="row" gap={4}>
+              <TextField
+                fullWidth
+                type="text"
+                variant="filled"
+                label="Employment Type"
+                {...register("employmentType")}
+              />
+              <TextField
+                fullWidth
+                type="text"
+                variant="filled"
+                label="Salary Range"
+                {...register("salaryRange")}
+              />
+            </Stack>
+            <Stack sx={{ my: 2 }} direction="row" gap={4}>
+              <TextField
+                fullWidth
+                type="text"
+                variant="filled"
+                label="Location"
+                {...register("location")}
+              />
+            </Stack>
+            <Stack sx={{ my: 2 }} direction="row" gap={4}>
+              <TextField
+                multiline
+                fullWidth
+                type="text"
+                variant="filled"
+                label="Overview"
+                {...register("overview")}
+              />
+            </Stack>
+            {/* skills  */}
+            <Box sx={{ my: 2 }}>
+              <Typography variant="subtitle1">Skills</Typography>
+              <Box>
+                {skillFields.map((item, index) => {
+                  return (
+                    <Stack
+                      sx={{ mb: 2 }}
+                      key={item.key}
+                      direction="row"
+                      gap={2}
+                    >
+                      <TextField
+                        fullWidth
+                        type="text"
+                        variant="filled"
+                        label="Skill"
+                        {...register(`skills[${index}]`)}
+                      />
+                      <Button
+                        onClick={() => skillRemove(index)}
+                        sx={{ fontSize: 30 }}
+                        color="error"
+                        variant="text"
+                      >
+                        <TiDelete />
+                      </Button>
+                    </Stack>
+                  );
+                })}
+              </Box>
+
+              <Button
+                onClick={() => skillAppend("")}
+                sx={{ textTransform: "inherit", borderRadius: 8, my: 1 }}
+                variant="outlined"
+              >
+                Add Skill
+              </Button>
+            </Box>
+            {/* Responsibilities  */}
+            <Box sx={{ my: 2 }}>
+              <Typography variant="subtitle1">Responsibilities</Typography>
+              <Box>
+                {responsibilityFields.map((item, index) => {
+                  return (
+                    <Stack
+                      sx={{ mb: 2 }}
+                      key={item.key}
+                      direction="row"
+                      gap={2}
+                    >
+                      <TextField
+                        fullWidth
+                        type="text"
+                        variant="filled"
+                        label="Responsibility"
+                        {...register(`responsibilities[${index}]`)}
+                      />
+                      <Button
+                        onClick={() => responsibilityRemove(index)}
+                        sx={{ fontSize: 30 }}
+                        color="error"
+                        variant="text"
+                      >
+                        <TiDelete />
+                      </Button>
+                    </Stack>
+                  );
+                })}
+              </Box>
+
+              <Button
+                onClick={() => responsibilityAppend("")}
+                sx={{ textTransform: "inherit", borderRadius: 8, my: 1 }}
+                variant="outlined"
+              >
+                Add Responsibility
+              </Button>
+            </Box>
+            {/* Requirements  */}
+            <Box sx={{ my: 2 }}>
+              <Typography variant="subtitle1">Requirements</Typography>
+              <Box>
+                {requirementFields.map((item, index) => {
+                  return (
+                    <Stack
+                      sx={{ mb: 2 }}
+                      key={item.key}
+                      direction="row"
+                      gap={2}
+                    >
+                      <TextField
+                        fullWidth
+                        type="text"
+                        variant="filled"
+                        label="Requirement"
+                        {...register(`requirements[${index}]`)}
+                      />
+                      <Button
+                        onClick={() => requirementRemove(index)}
+                        sx={{ fontSize: 30 }}
+                        color="error"
+                        variant="text"
+                      >
+                        <TiDelete />
+                      </Button>
+                    </Stack>
+                  );
+                })}
+              </Box>
+
+              <Button
+                onClick={() => requirementAppend("")}
+                sx={{ textTransform: "inherit", borderRadius: 8, my: 1 }}
+                variant="outlined"
+              >
+                Add Requirement
+              </Button>
+            </Box>
+
+            <Box sx={{ my: 3 }}>
+              <Button
+                sx={{ textTransform: "inherit" }}
+                type="submit"
+                variant="contained"
+                endIcon={<AiOutlineSend />}
+              >
+                Add Position
+              </Button>
+            </Box>
+          </form>
+        </Box>
+      </Container>
+    </>
+  );
 };
 
 export default AddJob;
