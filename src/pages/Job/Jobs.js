216025acbf1dc98badb6b2d918c88970f857f0f6
developer-suhag <developersuhag@gmail.com>
2023-01-14 01:30:41 +0600

    posted time filter

--- src/pages/Job/Jobs.js
+++ src/pages/Job/Jobs.js
@@ -1,4 +1,4 @@
-import { Box, Container, LinearProgress, Typography } from "@mui/material";
+import { Container, Typography } from "@mui/material";
 import { useSelector } from "react-redux";
 import JobCard from "../../components/Job/JobCard";
 import JobFilter from "../../components/Job/JobFilter";
@@ -10,15 +10,48 @@ const Jobs = () => {
   const { employmentType, postedTime, salaryRange, remote } = useSelector(
     (state) => state.filter
   );
+  const now = new Date();
+  const twentyFourHoursAgo = new Date(
+    now.getTime() - 24 * 60 * 60 * 1000
+  ).toISOString();
+  const threeDaysAgo = new Date(
+    now.getTime() - 3 * 24 * 60 * 60 * 1000
+  ).toISOString();
+  const twoWeeksAgo = new Date(
+    now.getTime() - 14 * 24 * 60 * 60 * 1000
+  ).toISOString();
+  const oneMonthAgo = new Date(
+    now.getFullYear(),
+    now.getMonth() - 1,
+    now.getDate(),
+    now.getHours(),
+    now.getMinutes(),
+    now.getSeconds()
+  ).toISOString();
+  // function checkRecentDate(date) {
+
+  //   if (date > twentyFourHoursAgo.toISOString()) {
+  //     return true;
+  //   } else if (date > threeDaysAgo.toISOString()) {
+  //     return true;
+  //   } else if (date > twoWeeksAgo.toISOString()) {
+  //     return true;
+  //   } else if (date > oneMonthAgo.toISOString()) {
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+  // }
 
   let content;
-  if (isLoading) {
-    content = (
-      <Box>
-        <LinearProgress />
-      </Box>
-    );
-  } else if (!isLoading && isError) {
+  // if (isLoading) {
+  //   content = (
+  //     <Box>
+  //       <LinearProgress />
+  //     </Box>
+  //   );
+  // } else
+  if (!isLoading && isError) {
     content = (
       <Typography sx={{ textAlign: "center" }} variant="h5" color="error">
         Something went wrong
@@ -39,11 +72,24 @@ const Jobs = () => {
         ?.filter((job) => job.employmentType === employmentType)
         ?.map((job) => <JobCard key={job._id} job={job} />);
     }
-    // if (postedTime !== "any-time") {
-    //   content = data?.data
-    //     ?.filter((job) => job.postedTime === postedTime)
-    //     ?.map((job) => <JobCard key={job._id} job={job} />);
-    // }
+    if (postedTime !== "any-time") {
+      // filter jobs by postedTime - 1 day, 3 days, 2 weeks, 1 month
+      content = data?.data
+        ?.filter((job) => {
+          if (postedTime === "last-day") {
+            return job.createdDate > twentyFourHoursAgo;
+          } else if (postedTime === "last-3-days") {
+            return job.createdDate > threeDaysAgo;
+          }
+          if (postedTime === "last-2-weeks") {
+            return job.createdDate > twoWeeksAgo;
+          } else if (postedTime === "last-month") {
+            return job.createdDate > oneMonthAgo;
+          }
+          return job;
+        })
+        ?.map((job) => <JobCard key={job._id} job={job} />);
+    }
     if (remote) {
       content = data?.data
         ?.filter((job) => job?.workType === "remote")
